64321c830cc82c32ffa94a4bba2db894316d4256
miguellimdofieb <miguelsantos.1402@aluno.barueri.br>
2025-03-31 08:28:59 -0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,44 @@ import {useState} from "react"
 
 function App(){
 
+ 
+const Formulário = (props) => {
+
   const [nome, setNome] = useState('Miguel')
+
 
   return (
     <div>
-      <h3>Pizzaria 28</h3>
+      <h3>{props.titulo}</h3>
       <input className="nome"
       placeholder="digite seu nome..."
       onChange={(e)=>{setNome(e.target.value)}}
       name="nome"
       type="text" />
-      <button className="botao" onClick={ ()=>alert(nome)}>
+      <button className="botao" onClick={ 
+        () => {
+          if(nome.lenght > 0)
+        alert(nome)
+      else
+      alert(props.mensagem)
+    }
+  }>
+
+
+
         CLIQUE AQUI
        </button>
+      </div>
+  )
+}
+
+  return (
+    <div>
+      <h3>Pizzaria 2B</h3>
+      <Formulário titulo = "Nome" mensagem = "sardinhas" />
+      <Formulário titulo = "E-mail" mensagem = "Coringão maior do Br"/>
+      <Formulário titulo = "CPF" mensagem = "sem mundial"/> 
+      <Formulário />
       </div>
   ) 
 }
