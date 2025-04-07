68beb345821224b755658b6fccf25fb96f900e37
artileiro de itakera <rm94102@estudante.fieb.edu.br>
2025-04-07 11:08:23 +0000

    alteração vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
-import {useState} from "react"
-
-function App(){
-
+import { useState } from "react"
  
-const Formulário = (props) => {
-
-  const [nome, setNome] = useState('Miguel')
-
-
+function App() {
+ 
+ 
+ 
+  const Formulario = (props) => {
+ 
+    const [nome, setNome] = useState('Ronaldo')
+ 
+    return (
+      <div>
+        <h3>{props.titulo}</h3>
+        <input className="nome"
+          placeholder="Digite seu nome..."
+          onChange={(e) => { setNome(e.target.value) }}
+          name="nome"
+          type="text" />
+        <button className="botao" onClick={
+                () => {
+                  if(nome.length > 0)
+                    alert(nome)
+                  else
+                    alert(props.mensagem)
+                }
+              }>
+          CLIQUE AQUI
+        </button>
+      </div>
+    )
+  }
+ 
   return (
     <div>
-      <h3>{props.titulo}</h3>
-      <input className="nome"
-      placeholder="digite seu nome..."
-      onChange={(e)=>{setNome(e.target.value)}}
-      name="nome"
-      type="text" />
-      <button className="botao" onClick={ 
-        () => {
-          if(nome.lenght > 0)
-        alert(nome)
-      else
-      alert(props.mensagem)
-    }
-  }>
-
-
-
-        CLIQUE AQUI
-       </button>
-      </div>
+      <h3> Pizzaria 2B</h3>
+      <Formulario titulo = "Nome" mensagem = "Santos" />
+      <Formulario titulo = "E-mail" mensagem = "Curintia" />
+      <Formulario titulo = "CPF" mensagem = "Parmera" />
+      <Formulario />
+ 
+    </div>
   )
 }
-
-  return (
-    <div>
-      <h3>Pizzaria 2B</h3>
-      <Formulário titulo = "Nome" mensagem = "sardinhas" />
-      <Formulário titulo = "E-mail" mensagem = "Coringão maior do Br"/>
-      <Formulário titulo = "CPF" mensagem = "sem mundial"/> 
-      <Formulário />
-      </div>
-  ) 
-}
-
+ 
 export default App
+ 
+ 
